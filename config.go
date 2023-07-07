c90b28b20d097f7afb477bb18ea53d24add617c5
bonedaddy <boned4ddy@protonmail.com>
2023-07-06 22:53:15 -0700

    update default modules

--- config.go
+++ config.go
@@ -6,11 +6,15 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/types/module"
 	"github.com/cosmos/cosmos-sdk/x/auth"
+	authz "github.com/cosmos/cosmos-sdk/x/authz/module"
 	"github.com/cosmos/cosmos-sdk/x/bank"
 	"github.com/cosmos/cosmos-sdk/x/crisis"
 	"github.com/cosmos/cosmos-sdk/x/distribution"
 	"github.com/cosmos/cosmos-sdk/x/gov"
+	"github.com/cosmos/cosmos-sdk/x/gov/client"
+	"github.com/cosmos/cosmos-sdk/x/mint"
 	"github.com/cosmos/cosmos-sdk/x/params"
+	paramsclient "github.com/cosmos/cosmos-sdk/x/params/client"
 	"github.com/cosmos/cosmos-sdk/x/slashing"
 	"github.com/cosmos/cosmos-sdk/x/staking"
 )
@@ -19,12 +23,16 @@ var (
 	// Provides a default set of modules
 	ModuleBasics = []module.AppModuleBasic{
 		auth.AppModuleBasic{},
+		authz.AppModuleBasic{},
 		bank.AppModuleBasic{},
 		// TODO: add osmosis governance proposal types here
 		// TODO: add other proposal types here
-		gov.NewAppModuleBasic(nil),
+		gov.NewAppModuleBasic([]client.ProposalHandler{
+			paramsclient.ProposalHandler,
+		}),
 		crisis.AppModuleBasic{},
 		distribution.AppModuleBasic{},
+		mint.AppModuleBasic{},
 		params.AppModuleBasic{},
 		slashing.AppModuleBasic{},
 		staking.AppModuleBasic{},
